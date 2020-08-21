adfa5e858b16d81d6fed882a46321a457678b06a
Felipe Tomao <fetomao@hotmail.com>
2020-08-21 16:50:49 -0300

    Setting up DB and structure

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+
+
 const app = express();
 
-app.get('/', (req, res) => {
-    return res.json({nessage: "Hello World"});
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 });
+
+app.use(express.json());
+app.use(routes);
+
 app.listen(3333);
