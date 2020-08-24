8495ed88a8637c2919c2a66ea7ff687f0335df6c
Felipe Tomao <fetomao@hotmail.com>
2020-08-24 18:29:59 -0300

    spot register

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,8 +1,14 @@
 const express = require('express');
-const SessionController = require ('./controllers/SessionController');
-const routes = express.Router();
+const multer = require('multer');
+const uploadConfig = require('./config/upload');
 
+const SessionController = require ('./controllers/SessionController');
+const SpotController = require ('./controllers/SpotController');
+
+const routes = express.Router();
+const upload = multer(uploadConfig);
 
 routes.post('/users', SessionController.store);
+routes.post('/spots', upload.single('thumbnail'), SpotController.store);
 
 module.exports = routes;
